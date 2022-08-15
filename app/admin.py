91efc8c311e32f0f7f9de77c352dfe7204fc3c12
Tuan Bach Van <bachvtuan@gmail.com>
2022-08-15 12:04:01 +0700

    closed #1

--- app/admin.py
+++ app/admin.py
@@ -1,34 +1,43 @@
 from django.contrib import admin
 from app.models import LogResult, Project, ProjectAPIKey
 from jutil.admin import ModelAdminBase
+from rest_framework_api_key.models import APIKey
+from rest_framework_api_key.admin import APIKeyModelAdmin
 
 
 class ProjectAdmin(ModelAdminBase):
-    list_display = (
-        "name",
-        "active",
-        "created"
-    )
+    list_display = ("name", "active", "created")
 
     list_filter = ("name",)
 
 
-class ProjectAPIKeyAdmin(ModelAdminBase):
-    list_display = (
-        "project",
-        "apikey",
-        "created"
-    )
-
-
 class LogResultAdmin(ModelAdminBase):
-    list_display = (
-        "project",
-        "created",
-        "status_code"
+    list_display = ("project", "created", "status_code")
+
+
+class ProjectAPIKeyInlineAdmin(admin.StackedInline):
+    model = ProjectAPIKey
+    can_delete = False
+    min_num = 1
+    max_num = 1
+    readonly_fields = ("created",)
+    raw_id_fields = ()
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ["project"],
+            },
+        ),
     )
 
+
+class CustomAPIKeyAdmin(APIKeyModelAdmin):
+    inlines = (ProjectAPIKeyInlineAdmin,)
+
+
+admin.site.unregister(APIKey)
+admin.site.register(APIKey, CustomAPIKeyAdmin)
 
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(ProjectAPIKey, ProjectAPIKeyAdmin)
 admin.site.register(LogResult, LogResultAdmin)
